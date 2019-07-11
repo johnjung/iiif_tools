087426fe423d2e6616e769ba9de2e715439261bd
Jose Reyes <reyesj5@UCHICAGO.EDU>
2019-07-11 14:12:46 -0500

    adding file functionality to iiif_tools

--- iiif_tools/__main__.py
+++ iiif_tools/__main__.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 """Usage:
-    iiif_tools -
+    iiif_tools -f <path>
+
+Options:
+  -h --help     Show this screen.
+  -f --file     File path to make manifest from
 """
 
 
 import sys
 from docopt import docopt
-from builders import MapsIIIFManifest
+from maps_manifest import MapsIIIFManifest
 
 if __name__=="__main__":
   options = docopt(__doc__)
-
-  if "-" in options:
-    dcxml = sys.stdin.read()
-
-  sys.stdout.write(str(MapsIIIFManifest(dcxml)))
+  if "<path>" in options:
+  	with open(options['<path>'], 'r') as dcxml:
+	  	sys.stdout.write(str(MapsIIIFManifest(dcxml.read())))
+  else:
+  	dcxml = sys.stdin.read()
+  	sys.stdout.write(str(MapsIIIFManifest(dcxml)))
